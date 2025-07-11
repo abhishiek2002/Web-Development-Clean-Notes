fd86bed48f25537d4dfaa87360e51341d397e593
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-07-11 08:33:30 +0530

    Library Task

---
+++ Express/07 Router and Filters - Task (Library System)/Routers/Books.js
@@ -0,0 +1,14 @@
+import {Router} from 'express'
+
+const router = Router();
+
+router
+  .route("")
+  .get((req, res) => {
+    res.send("Here is the list of books.");
+  })
+  .post((req, res) => {
+    res.send("Book has been added!");
+  });
+
+  export default router;
---
+++ Express/07 Router and Filters - Task (Library System)/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bookRouter from "./Routers/Books.js"
+
+const app = express();
+let port = 4000;
+
+app.use((req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+
+  console.log(`${method} request made to ${url}`);
+  next();
+});
+
+app.use("/books", bookRouter);
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
+})
+
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`)
+);
